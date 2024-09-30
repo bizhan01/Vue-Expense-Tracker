906f65867c680dcc5e8e419a002c1388afd3ad7f
bizhan01 <rahmatullahbizhan@gmail.com>
2024-09-30 12:32:42 +0430

    Add Transaction Coponents created

--- src/components/AddTransaction.vue
+++ src/components/AddTransaction.vue
@@ -2,5 +2,19 @@
 </script>
 
 <template>
-     <h2>Expense Tracker</h2>
+    <h3>Add new transaction</h3>
+      <form id="form">
+        <div class="form-control">
+          <label for="text">Text</label>
+          <input type="text" id="text" placeholder="Enter text..." />
+        </div>
+        <div class="form-control">
+          <label for="amount"
+            >Amount <br />
+            (negative - expense, positive - income)</label
+          >
+          <input type="number" id="amount" placeholder="Enter amount..." />
+        </div>
+        <button class="btn">Add transaction</button>
+      </form>
 </template>
