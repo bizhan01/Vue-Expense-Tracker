d1f3cd63411fdc29c4fc2b434ee73a44fca1fd22
bizhan01 <rahmatullahbizhan@gmail.com>
2024-10-01 10:28:32 +0430

    Daynamic Balance Setup

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import Balance from './components/Balance.vue';
 import IncomeExpenses from './components/IncomeExpenses.vue';
 import TransactionList from './components/TransactionList.vue';
 import AddTransaction from './components/AddTransaction.vue';
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 
 const transactions = ref ([
   { id: 1, text: 'Flower', amount: -19.99 },
@@ -12,12 +12,18 @@ const transactions = ref ([
   { id: 3, text: 'Book', amount: -10 },
   { id: 4, text: 'Camera', amount: 150 },
 ]);
+
+const total = computed (() => {
+  return transactions.value.reduce((acc, transaction) => {
+    return acc + transaction.amount;
+  }, 0);
+});
 </script>
 
 <template>
   <Header />
   <div class="container">
-      <Balance />
+      <Balance :total="total"/>
       <IncomeExpenses />
       <TransactionList :transactions="transactions"/>
       <AddTransaction />
